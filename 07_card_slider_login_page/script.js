8adea6eada885a0a3e1c7b9eb1faa7121ad432d8
palixarjun <palixarjun@gmail.com>
2024-08-20 22:08:31 +0530

    script

---
+++ 07_card_slider_login_page/script.js
@@ -0,0 +1,14 @@
+// Selecting the buttons and container by their IDs
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+// Adding event listener for the 'Sign Up' button
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active"); // Add 'active' class to container
+});
+
+// Adding event listener for the 'Sign In' button
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active"); // Remove 'active' class from container
+});
