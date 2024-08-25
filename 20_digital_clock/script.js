6ae40d136b04305ac4011891b856f3b19d9489c5
palixarjun <palixarjun@gmail.com>
2024-08-25 16:22:08 +0530

    simple-digital-clock

---
+++ 20_digital_clock/script.js
@@ -0,0 +1,32 @@
+const hoursEl = document.getElementById("hours");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+
+
+function updateClock()  {
+    const dateFunction = new Date();
+
+    // console.log(dateFunction);
+
+    let hours = dateFunction.getHours();
+    let minutes = dateFunction.getMinutes();
+    let seconds = dateFunction.getSeconds();
+
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    hoursEl.innerText = hours;
+    minutesEl.innerText = minutes;
+    secondsEl.innerText = seconds;
+
+    setTimeout(() => {
+        updateClock();
+    }, 1000);
+
+};
+
+updateClock();
+
+
+
